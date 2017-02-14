5086089ab99a9285f52b76a502b09d4347fe7827
Anas Saeed <anassaeed72@gmail.com>
2017-02-14 11:31:01 -0600

    Sync

--- Server.java
+++ Server.java
@@ -8,9 +8,11 @@ public class Server{
 	public static void main(String[] args) {
 		System.out.println("In main");
 		Server server =   new Server();
+		print("\n\n\n\nCreating Account\n\n\n\n");
 		for (int i = 0;i < 100 ;i++ ) {
 			print(server.CreateAcount());
 		}
+		print("\n\n\n\nDepositing Money\n\n\n\n\n");
 		int sum  = 0;
 		for (int i = 0;i < 100 ;i++ ) {
 			print(server.Deposit(i,100));
@@ -19,8 +21,56 @@ public class Server{
 		for (int i = 0;i < 100 ;i++ ) {
 			sum+=(server.Getbalance(i));
 		}
+		print("\n\n\n\nSumming Deposits\n\n\n\n");
 		print (sum);
-		
+		int numberofThreads = 1;
+		int numberofIterations = 10;
+		ArrayList<Thread> threadsList = new ArrayList<Thread>();
+		print("\n\n\n\nStarting Thread\n\n\n\n");
+		for (int i = 0;i <numberofThreads ;i++ ) {
+			Thread thread = new Thread(new Runnable() {
+
+			    @Override
+			    public void run() {
+			    			// Server server =   new Server();
+			    	for (int j = 0;j<numberofIterations ;j++ ) {
+				        Random rand = new Random(); 
+				        int  sourceAccount =(int) rand.nextInt(99);
+				        int depositAccount = (int)rand.nextInt(99);
+				        sourceAccount = j;
+				        print("sourceAccount");
+				        print(sourceAccount);
+				        print("depositAccount");
+				        print(depositAccount);
+				        print(server.Transfer(sourceAccount, depositAccount, 10));
+				     
+
+				    }
+
+			    }
+			            
+			});
+			        
+			thread.start();
+			threadsList.add(thread);
+		}
+
+		for (Thread thread : threadsList ) {
+			try{
+			thread.join();
+
+			}catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+
+		print("All Done");
+		sum =0;
+		for (int i = 0;i < 100 ;i++ ) {
+			sum+=(server.Getbalance(i));
+		}
+		print("\n\n\n\nFinal Sum\n\n\n\n\n");
+		print (sum);
 
 	}
 	int CreateAcount(){
@@ -52,26 +102,36 @@ public class Server{
 	}
 
 	String Transfer(int source, int dest, int amount){
-		Account account  =(Account) accounts.get(source);
+		Account sourceAccount  =(Account) accounts.get(source);
 		Account depositAccount  =(Account) accounts.get(dest);
-
-		while (account.lock.tryLock() == false && depositAccount.lock.tryLock()){}
-		if (account.withdral(amount)) {
+		if (sourceAccount == null) {
+			System.out.println("source account is null");
+			return "FAIL";
+		}
+		if (depositAccount == null) {
+			System.out.println("depositAccount is null");
+			return "FAIL";
+			
+		}
+		while (sourceAccount.lock.tryLock() == false && depositAccount.lock.tryLock() == false){}
+		if (sourceAccount.withdral(amount)) {
 			depositAccount.deposit(amount);
-			account.lock.unlock();
+			sourceAccount.lock.unlock();
 			depositAccount.lock.unlock();
 			return "OK";
 		}
-		account.lock.unlock();
+		sourceAccount.lock.unlock();
 		depositAccount.lock.unlock();
 		return "FAIL";
 	}
 	public  static void print(String input){
+		
 		System.out.println(input);
 
 	}
 
 	public  static void print(int input){
+		
 		System.out.println(input);
 		
 	}
