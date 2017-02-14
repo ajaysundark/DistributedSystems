871213449c735277d9d3cdd6f268aa18f76b14fc
Anas Saeed <anassaeed72@gmail.com>
2017-02-14 15:13:56 -0600

    running

--- TestServer.java
+++ TestServer.java
@@ -14,7 +14,7 @@ public class TestServer {
    boolean ServerOn = true;
    public TestServer() { 
       try {
-         myServerSocket = new ServerSocket(8889);
+         myServerSocket = new ServerSocket(1589);
       } catch(IOException ioe) { 
          System.out.println("Could not create server socket on port 8888. Quitting.");
          System.exit(-1);
