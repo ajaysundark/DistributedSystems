206f9d1eba2b1bfb88a4500989c804ac8944ce25
Anas Saeed <anassaeed72@gmail.com>
2017-02-14 14:25:57 -0600

    running

--- TestServer.java
+++ TestServer.java
@@ -5,29 +5,23 @@ class TestServer {
     ServerSocket serverSocket;
     int serverPortNumber = 8888;
     boolean serverRunning = true;
-    public static void main(String[] args) {
+    TestServer(){
+    	try {
+            serverSocket = new ServerSocket(serverPortNumber);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        Thread mainThread = new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                try {
-                    serverSocket = new ServerSocket(serverPortNumber);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-                while (serverRunning) {
-                    try {
-                        Socket clientSocket = ServerSocket.accept();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-
+        while (serverRunning) {
+            try {
+                Socket clientSocket = ServerSocket.accept();
+                // then do something witht the socket
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-
-        });
-        mainThread.start();
+        }
+    }
+    public static void main(String[] args) {
+		new  TestServer();
     }
 }
