5c80cdfab62bee312047d649f0fea8bd9b3cd153
Anas Saeed <anassaeed72@gmail.com>
2017-02-14 05:42:55 -0600

    Implemented account details

--- Account.java
+++ Account.java
@@ -2,6 +2,32 @@ public class Account {
 	int balance;
 	int uid;
 	Account(){
-		
+
+	}
+	int getBalance(){
+		return balance;
+	}
+	int getUID(){
+		return uid;
+	}
+	void setUID(int _uid){
+		uid = _uid;
+	}
+	void setBalance(int _balance){
+		balance = _balance;
+	}
+
+	boolean deposit(int _amount){
+		balance+=_amount
+		return true;
+	}
+
+	boolean withdral(int _amount){
+		if (balance-_amount <0) {
+			return false;
+		}
+
+		balance-=_amount;
+		return true;
 	}
 }
