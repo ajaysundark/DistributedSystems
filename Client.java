5ecfddf4ac7a442c30bc5975db21558cea7116b1
Anas Saeed <anassaeed72@gmail.com>
2017-02-14 15:15:33 -0600

    running

--- Client.java
+++ Client.java
@@ -4,7 +4,6 @@ import java.io.*;
 
 
 public class Client{
-	public static void main(String[] args) {
 
 		public static void main(String [] args) {
       String serverName = "127.0.0.1";//args[0];
