c46a20470db2c441508b0adeaa76db2b3bcfdfba
Anas Saeed <anassaeed72@gmail.com>
2017-02-16 14:51:33 -0600

    running

--- TestServer.java
+++ TestServer.java
@@ -1,190 +1,58 @@
- import java.net.*;
+import java.net.*;
 import java.io.*;
 
-class TestServer {
-    ServerSocket serverSocket;
-    int serverPortNumber = 7890;
-    boolean serverRunning = true;
-    public TestServer(){
-    	print("Starting Server");
-    	try {
-            serverSocket = new ServerSocket(serverPortNumber);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+public class TestServer extends Thread {
+   private ServerSocket serverSocket;
+   
+   public TestServer(int port) throws IOException {
+      serverSocket = new ServerSocket(port);
+      serverSocket.setSoTimeout(10000);
+   }
 
-        while (serverRunning) {
-        	print("In while loop");
-            try {
-                final Socket clientSocket = serverSocket.accept();
-                Thread thread = new Thread(new Runnable() {
-                	Socket clientSocketInsideThread = clientSocket;
-				    @Override
-				    public void run() {
-				    	print("in run func");
-				    	BufferedReader inputStream = null; 
-        				PrintWriter outputStream = null; 
-				    	try{
-
-				    		inputStream = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            				outputStream = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-            				if (inputStream == null || outputStream == null) {
-            					print ("streams are null");
-            				}
-            				while(true){
-            					print("listening to data stream");
-            					String clientCommand = inputStream.readLine();
-            					System.out.println("Client Says :" + clientCommand);
-            					outputStream.print("Oka");
-            				}
-				    	}catch (Exception e) {
-				    		e.printStackTrace();
-				    	}
-				    }
-				            
-				});
-				thread.start();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-    public static void main(String[] args) {
-		new  TestServer();
-    }
-
-    public  static void print(String input){
-		
-		System.out.println(input);
-
-	}
-
-	public  static void print(int input){
-		
-		System.out.println(input);
-		
-	}
-}
-
-
-// import java.io.BufferedReader;
-// import java.io.IOException;
-// import java.io.InputStreamReader;
-// import java.io.OutputStreamWriter;
-// import java.io.PrintWriter;
-
-// import java.net.ServerSocket;
-// import java.net.Socket;
-// import java.text.SimpleDateFormat;
-// import java.util.Calendar;
- 
-// public class TestServer {
-//    ServerSocket myServerSocket;
-//    boolean ServerOn = true;
-//    public TestServer() { 
-//       try {
-//          myServerSocket = new ServerSocket(1567);
-//       } catch(IOException ioe) { 
-//          System.out.println("Could not create server socket on port 8888. Quitting.");
-//          System.exit(-1);
-//       } 
-		
-//       Calendar now = Calendar.getInstance();
-//       SimpleDateFormat formatter = new SimpleDateFormat(
-//          "E yyyy.MM.dd 'at' hh:mm:ss a zzz");
-//       System.out.println("It is now : " + formatter.format(now.getTime()));
-      
-//       while(ServerOn) { 
-//          try { 
-//             Socket clientSocket = myServerSocket.accept();
-//             ClientServiceThread cliThread = new ClientServiceThread(clientSocket);
-//             cliThread.start(); 
-//          } catch(IOException ioe) { 
-//             System.out.println("Exception found on accept. Ignoring. Stack Trace :"); 
-//             ioe.printStackTrace(); 
-//          }  
-//       } 
-//       try { 
-//          myServerSocket.close(); 
-//          System.out.println("Server Stopped"); 
-//       } catch(Exception ioe) { 
-//          System.out.println("Error Found stopping server socket"); 
-//          System.exit(-1); 
-//       } 
-//    }
-	
-//    public static void main (String[] args) { 
-//       new TestServer();        
-//    } 
-	
-//    class ClientServiceThread extends Thread { 
-//       Socket myClientSocket;
-//       boolean m_bRunThread = true; 
-//       public ClientServiceThread() { 
-//          super(); 
-//       } 
-		
-//       ClientServiceThread(Socket s) { 
-//          myClientSocket = s; 
-//       } 
-		
-//       public void run() { 
-//          BufferedReader in = null; 
-//          PrintWriter out = null; 
-//          System.out.println(
-//             "Accepted Client Address - " + myClientSocket.getInetAddress().getHostName());
-//          try { 
-//             in = new BufferedReader(
-//                new InputStreamReader(myClientSocket.getInputStream()));
-//             out = new PrintWriter(
-//                new OutputStreamWriter(myClientSocket.getOutputStream()));
+   public void run() {
+      while(true) {
+         try {
+            System.out.println("Waiting for client on port " + 
+               serverSocket.getLocalPort() + "...");
+            Socket server = serverSocket.accept();
             
-//             while(m_bRunThread) { 
-//             	System.out.println("In while loop");
-//                String clientCommand = in.readLine(); 
-//                System.out.println("Client Says :" + clientCommand);
-               
-//                if(!ServerOn) { 
-//                   System.out.print("Server has already stopped"); 
-//                   out.println("Server has already stopped"); 
-//                   out.flush(); 
-//                   m_bRunThread = false;
-//                } 
-//                if(clientCommand.equalsIgnoreCase("quit")) {
-//                   m_bRunThread = false;
-//                   System.out.print("Stopping client thread for client : ");
-//                } else if(clientCommand.equalsIgnoreCase("end")) {
-//                   m_bRunThread = false;
-//                   System.out.print("Stopping client thread for client : ");
-//                   ServerOn = false;
-//                } else {
-//                   out.println("Server Says : " + clientCommand);
-//                   out.flush(); 
-//                } 
-//             } 
-//          } catch(Exception e) { 
-//             e.printStackTrace(); 
-//          } 
-//          finally { 
-//             try { 
-//                in.close(); 
-//                out.close(); 
-//                myClientSocket.close(); 
-//                System.out.println("...Stopped"); 
-//             } catch(IOException ioe) { 
-//                ioe.printStackTrace(); 
-//             } 
-//          } 
-//       } 
-//    } 
-// }
+            System.out.println("Just connected to " + server.getRemoteSocketAddress());
+            DataInputStream in = new DataInputStream(server.getInputStream());
+            
+            System.out.println(in.readUTF());
+            DataOutputStream out = new DataOutputStream(server.getOutputStream());
+            out.writeUTF("Thank you for connecting to " + server.getLocalSocketAddress()
+               + "\nGoodbye!");
+            server.close();
+            
+         }catch(SocketTimeoutException s) {
+            System.out.println("Socket timed out!");
+            break;
+         }catch(IOException e) {
+            e.printStackTrace();
+            break;
+         }
+      }
+   }
+   
+   public static void main(String [] args) {
+      int port = Integer.parseInt(args[0]);
+      try {
+         Thread t = new TestServer(port);
+         t.start();
+      }catch(IOException e) {
+         e.printStackTrace();
+      }
+   }
+} 
 
-// import java.net.*;
+
+ // import java.net.*;
 // import java.io.*;
 
 // class TestServer {
 //     ServerSocket serverSocket;
-//     int serverPortNumber = 8870;
+//     int serverPortNumber = 7890;
 //     boolean serverRunning = true;
 //     public TestServer(){
 //     	print("Starting Server");
@@ -217,6 +85,7 @@ class TestServer {
 //             					String clientCommand = inputStream.readLine();
 //             					System.out.println("Client Says :" + clientCommand);
 //             					outputStream.print("Oka");
+//             					print("After data is echoed back to the Client");
 //             				}
 // 				    	}catch (Exception e) {
 // 				    		e.printStackTrace();
@@ -246,3 +115,184 @@ class TestServer {
 		
 // 	}
 // }
+
+
+// // import java.io.BufferedReader;
+// // import java.io.IOException;
+// // import java.io.InputStreamReader;
+// // import java.io.OutputStreamWriter;
+// // import java.io.PrintWriter;
+
+// // import java.net.ServerSocket;
+// // import java.net.Socket;
+// // import java.text.SimpleDateFormat;
+// // import java.util.Calendar;
+ 
+// // public class TestServer {
+// //    ServerSocket myServerSocket;
+// //    boolean ServerOn = true;
+// //    public TestServer() { 
+// //       try {
+// //          myServerSocket = new ServerSocket(1567);
+// //       } catch(IOException ioe) { 
+// //          System.out.println("Could not create server socket on port 8888. Quitting.");
+// //          System.exit(-1);
+// //       } 
+		
+// //       Calendar now = Calendar.getInstance();
+// //       SimpleDateFormat formatter = new SimpleDateFormat(
+// //          "E yyyy.MM.dd 'at' hh:mm:ss a zzz");
+// //       System.out.println("It is now : " + formatter.format(now.getTime()));
+      
+// //       while(ServerOn) { 
+// //          try { 
+// //             Socket clientSocket = myServerSocket.accept();
+// //             ClientServiceThread cliThread = new ClientServiceThread(clientSocket);
+// //             cliThread.start(); 
+// //          } catch(IOException ioe) { 
+// //             System.out.println("Exception found on accept. Ignoring. Stack Trace :"); 
+// //             ioe.printStackTrace(); 
+// //          }  
+// //       } 
+// //       try { 
+// //          myServerSocket.close(); 
+// //          System.out.println("Server Stopped"); 
+// //       } catch(Exception ioe) { 
+// //          System.out.println("Error Found stopping server socket"); 
+// //          System.exit(-1); 
+// //       } 
+// //    }
+	
+// //    public static void main (String[] args) { 
+// //       new TestServer();        
+// //    } 
+	
+// //    class ClientServiceThread extends Thread { 
+// //       Socket myClientSocket;
+// //       boolean m_bRunThread = true; 
+// //       public ClientServiceThread() { 
+// //          super(); 
+// //       } 
+		
+// //       ClientServiceThread(Socket s) { 
+// //          myClientSocket = s; 
+// //       } 
+		
+// //       public void run() { 
+// //          BufferedReader in = null; 
+// //          PrintWriter out = null; 
+// //          System.out.println(
+// //             "Accepted Client Address - " + myClientSocket.getInetAddress().getHostName());
+// //          try { 
+// //             in = new BufferedReader(
+// //                new InputStreamReader(myClientSocket.getInputStream()));
+// //             out = new PrintWriter(
+// //                new OutputStreamWriter(myClientSocket.getOutputStream()));
+            
+// //             while(m_bRunThread) { 
+// //             	System.out.println("In while loop");
+// //                String clientCommand = in.readLine(); 
+// //                System.out.println("Client Says :" + clientCommand);
+               
+// //                if(!ServerOn) { 
+// //                   System.out.print("Server has already stopped"); 
+// //                   out.println("Server has already stopped"); 
+// //                   out.flush(); 
+// //                   m_bRunThread = false;
+// //                } 
+// //                if(clientCommand.equalsIgnoreCase("quit")) {
+// //                   m_bRunThread = false;
+// //                   System.out.print("Stopping client thread for client : ");
+// //                } else if(clientCommand.equalsIgnoreCase("end")) {
+// //                   m_bRunThread = false;
+// //                   System.out.print("Stopping client thread for client : ");
+// //                   ServerOn = false;
+// //                } else {
+// //                   out.println("Server Says : " + clientCommand);
+// //                   out.flush(); 
+// //                } 
+// //             } 
+// //          } catch(Exception e) { 
+// //             e.printStackTrace(); 
+// //          } 
+// //          finally { 
+// //             try { 
+// //                in.close(); 
+// //                out.close(); 
+// //                myClientSocket.close(); 
+// //                System.out.println("...Stopped"); 
+// //             } catch(IOException ioe) { 
+// //                ioe.printStackTrace(); 
+// //             } 
+// //          } 
+// //       } 
+// //    } 
+// // }
+
+// // import java.net.*;
+// // import java.io.*;
+
+// // class TestServer {
+// //     ServerSocket serverSocket;
+// //     int serverPortNumber = 8870;
+// //     boolean serverRunning = true;
+// //     public TestServer(){
+// //     	print("Starting Server");
+// //     	try {
+// //             serverSocket = new ServerSocket(serverPortNumber);
+// //         } catch (Exception e) {
+// //             e.printStackTrace();
+// //         }
+
+// //         while (serverRunning) {
+// //         	print("In while loop");
+// //             try {
+// //                 final Socket clientSocket = serverSocket.accept();
+// //                 Thread thread = new Thread(new Runnable() {
+// //                 	Socket clientSocketInsideThread = clientSocket;
+// // 				    @Override
+// // 				    public void run() {
+// // 				    	print("in run func");
+// // 				    	BufferedReader inputStream = null; 
+// //         				PrintWriter outputStream = null; 
+// // 				    	try{
+
+// // 				    		inputStream = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+// //             				outputStream = new PrintWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+// //             				if (inputStream == null || outputStream == null) {
+// //             					print ("streams are null");
+// //             				}
+// //             				while(true){
+// //             					print("listening to data stream");
+// //             					String clientCommand = inputStream.readLine();
+// //             					System.out.println("Client Says :" + clientCommand);
+// //             					outputStream.print("Oka");
+// //             				}
+// // 				    	}catch (Exception e) {
+// // 				    		e.printStackTrace();
+// // 				    	}
+// // 				    }
+				            
+// // 				});
+// // 				thread.start();
+// //             } catch (Exception e) {
+// //                 e.printStackTrace();
+// //             }
+// //         }
+// //     }
+// //     public static void main(String[] args) {
+// // 		new  TestServer();
+// //     }
+
+// //     public  static void print(String input){
+		
+// // 		System.out.println(input);
+
+// // 	}
+
+// // 	public  static void print(int input){
+		
+// // 		System.out.println(input);
+		
+// // 	}
+// // }
