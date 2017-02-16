6c66f6f431c8cb9901014d8277a4045b5922aaf8
Anas Saeed <anassaeed72@gmail.com>
2017-02-16 14:33:45 -0600

    running

--- Server.java
+++ Server.java
@@ -4,59 +4,126 @@ import java.net.*;
 import java.io.*;
 
 public class Server{
-	Hashtable accounts= new Hashtable();
-	int base=0;
-	ReentrantLock serverLock = new ReentrantLock();
-		 ServerSocket serverSocket;
-    int serverPortNumber = 8888;
-    boolean serverRunning = true;
+	// Hashtable accounts= new Hashtable();
+	// int base=0;
+	// ReentrantLock serverLock = new ReentrantLock();
+	// 	 ServerSocket serverSocket;
+ //    int serverPortNumber = 8888;
+ //    boolean serverRunning = true;
 
 	
 	
-    Server(){
-    	print("Creating server");
-    	try {
-            serverSocket = new ServerSocket(serverPortNumber);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+ //    Server(){
+ //    	print("Creating server");
+ //    	try {
+ //            serverSocket = new ServerSocket(serverPortNumber);
+ //        } catch (Exception e) {
+ //            e.printStackTrace();
+ //        }
 
-        while (serverRunning) {
-            try {
-                final Socket clientSocket = serverSocket.accept();
-                print("got connection");
-                Thread thread = new Thread(new Runnable() {
-                	Socket clientSocketInsideThread = clientSocket;
-				    @Override
-				    public void run() {
-				    	BufferedReader inputStream = null; 
-        				PrintWriter outputStream = null; 
-				    	print("TestServer");
-				    	try{
+ //        while (serverRunning) {
+ //            try {
+ //                final Socket clientSocket = serverSocket.accept();
+ //                print("got connection");
+ //                Thread thread = new Thread(new Runnable() {
+ //                	Socket clientSocketInsideThread = clientSocket;
+	// 			    @Override
+	// 			    public void run() {
+	// 			    	BufferedReader inputStream = null; 
+ //        				PrintWriter outputStream = null; 
+	// 			    	print("TestServer");
+	// 			    	try{
 
-				    		 inputStream = new BufferedReader(
-               new InputStreamReader(clientSocketInsideThread.getInputStream()));
-            outputStream = new PrintWriter(
-               new OutputStreamWriter(clientSocketInsideThread.getOutputStream()));
-            				while(true){
+	// 			    		 inputStream = new BufferedReader(
+ //               new InputStreamReader(clientSocketInsideThread.getInputStream()));
+ //            outputStream = new PrintWriter(
+ //               new OutputStreamWriter(clientSocketInsideThread.getOutputStream()));
+ //            				while(true){
             					
-            					String clientCommand = inputStream.readLine();
-            					print("Client Command " + clientCommand);
-            				}
-				    	}catch (Exception e) {
-				    		e.printStackTrace();
-				    	}
-				    }
+ //            					String clientCommand = inputStream.readLine();
+ //            					print("Client Command " + clientCommand);
+ //            				}
+	// 			    	}catch (Exception e) {
+	// 			    		e.printStackTrace();
+	// 			    	}
+	// 			    }
 				            
-				});
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+	// 			});
+ //            } catch (Exception e) {
+ //                e.printStackTrace();
+ //            }
+ //        }
+
+
 
 	}
 	public static void main(String[] args) {
-		new Server();
+		// new Server();
+
+			System.out.println("In main");
+		Server server =   new Server();
+		print("\n\n\n\nCreating Account\n\n\n\n");
+		for (int i = 0;i < 100 ;i++ ) {
+			print(server.CreateAcount());
+		}
+		print("\n\n\n\nDepositing Money\n\n\n\n\n");
+		int sum  = 0;
+		for (int i = 0;i < 100 ;i++ ) {
+			print(server.Deposit(i,100));
+		}
+
+		for (int i = 0;i < 100 ;i++ ) {
+			sum+=(server.Getbalance(i));
+		}
+		print("\n\n\n\nSumming Deposits\n\n\n\n");
+		print (sum);
+		int numberofThreads = 1;
+		int numberofIterations = 0;
+		ArrayList<Thread> threadsList = new ArrayList<Thread>();
+		print("\n\n\n\nStarting Thread\n\n\n\n");
+		for (int i = 0;i <numberofThreads ;i++ ) {
+			Thread thread = new Thread(new Runnable() {
+
+			    @Override
+			    public void run() {
+			    	for (int j = 0;j<numberofIterations ;j++ ) {
+				        Random rand = new Random(); 
+				        int  sourceAccount =(int) rand.nextInt(99);
+				        int depositAccount = (int)rand.nextInt(99);
+				        // sourceAccount = j;
+				        print("sourceAccount");
+				        print(sourceAccount);
+				        print("depositAccount");
+				        print(depositAccount);
+				        print(server.Transfer(sourceAccount, depositAccount, 10));
+
+
+				    }
+
+			    }
+			            
+			});
+			        
+			thread.start();
+			threadsList.add(thread);
+		}
+
+		for (Thread thread : threadsList ) {
+			try{
+			thread.join();
+
+			}catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+
+		print("All Done");
+		sum =0;
+		for (int i = 0;i < 100 ;i++ ) {
+			sum+=(server.Getbalance(i));
+		}
+		print("\n\n\n\nFinal Sum\n\n\n\n\n");
+		print (sum);
 	}
 	int CreateAcount(){
 		while(serverLock.tryLock() == false){}
